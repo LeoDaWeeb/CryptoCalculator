6722f8f758f54a25e440a6efc4b35c9ec43efe07
LeoDaWeeb <j13.laios@outlook.com.gr>
2021-11-24 21:07:10 +0200

    commit on master

--- js/calculator.js
+++ js/calculator.js
@@ -45,55 +45,17 @@ function calculateProfitability(gpuPrice, hashrate, powerConsumption, costPerKwh
     }
 }
 
-function renderError(error) {
-    
+function renderError(elementId, error) {
+    document.getElementById(elementId).textContent = error
+}
+
+function deleteErrorMessages() {
+    ['gpuprice', 'hashrate', 'power', 'elcost'].forEach(elementId => {
+        document.getElementById(`${elementId}-error-text`).textContent = "ㅤ"
+    })
 }
 
 function renderCalculatedValues(values) {
-
-}
-
-function calculator() {
-    let gpuPriceValue = document.getElementsByName('gpuprice')[0].value
-    let hashrateValue = document.getElementsByName('hashrate')[0].value
-    let powerValue = document.getElementsByName('power')[0].value
-    let elCostValue = document.getElementsByName('elcost')[0].value
-    
-    let errorOccured = false
-    
-    if (isNaN(Number(gpuPriceValue))) {
-        renderError('gpuprice')
-        errorOccured = true
-    }
-    else {
-        gpuPriceValue = Number(gpuPriceValue)
-    }
-    if (isNaN(Number(hashrateValue))) {
-        renderError('hashrate')
-        errorOccured = true
-    }
-    else {
-        hashrateValue = Number(hashrateValue)
-    }
-    if (isNaN(Number(powerValue))) {
-        renderError('hashrate')
-        errorOccured = true
-    }
-    else {
-        powerValue = Number(powerValue)
-    }
-    if (isNaN(Number(elCostValue))) {
-        renderError('elcost')
-        errorOccured = true
-    }
-    else {
-        elCostValue = Number(elCostValue)
-    }
-
-    if (errorOccured) { alert(400); return }
-
-    let values = calculateProfitability(gpuPriceValue, hashrateValue, powerValue, elCostValue)
-
     document.getElementById('perday-income-col').textContent = values.results.perday.income.toFixed(2) + '€'
     document.getElementById('perday-elcost-col').textContent = values.results.perday.elCosts.toFixed(2) + '€'
     document.getElementById('perday-profit-col').textContent = values.results.perday.profit.toFixed(2) + '€'
@@ -115,4 +77,94 @@ function calculator() {
     document.getElementById('result-table').style.display = 'block'
 
     document.getElementById('table').scrollIntoView(true, {behavior: 'smooth'})
+}
+
+function calculator() {
+    let gpuPriceValue = document.getElementsByName('gpuprice')[0].value
+    let hashrateValue = document.getElementsByName('hashrate')[0].value
+    let powerValue = document.getElementsByName('power')[0].value
+    let elCostValue = document.getElementsByName('elcost')[0].value
+    
+    let errorOccured = false
+
+    if (gpuPriceValue === "") {
+        renderError('gpuprice-error-text', 'GPU price cannot be empty!')
+        errorOccured = true
+    }
+    else if (isNaN(Number(gpuPriceValue))) {
+        renderError('gpuprice-error-text', 'GPU price must be a number!')
+        errorOccured = true
+    }
+    else {
+        if (Number(gpuPriceValue) < 0) {
+            renderError('gpuprice-error-text', 'GPU price cannot have a negative value!')
+            errorOccured = true
+        }
+        else {
+            gpuPriceValue = Number(gpuPriceValue)
+        }
+    }
+
+    if (hashrateValue === "") {
+        renderError('hashrate-error-text', 'Hashrate cannot be empty!')
+        errorOccured = true
+    }
+    else if (isNaN(Number(hashrateValue))) {
+        renderError('hashrate-error-text', 'Hashrate must be a number!')
+        errorOccured = true
+    }
+    else {
+        if (Number(hashrateValue) < 0) {
+            renderError('hashrate-error-text', 'Hashrate cannot have a negative value!')
+            errorOccured = true
+        }
+        else {
+            hashrateValue = Number(hashrateValue)
+        }
+    }
+
+    if (powerValue === "") {
+        renderError('power-error-text', 'Power consumption cannot be empty!')
+        errorOccured = true
+    }
+    else if (isNaN(Number(powerValue))) {
+        renderError('power-error-text', 'Power consumption must be a number!')
+        errorOccured = true
+    }
+    else {
+        if (Number(powerValue) < 0) {
+            renderError('power-error-text', 'Power consumption cannot have a negative value!')
+            errorOccured = true
+        }
+        else {
+            powerValue = Number(powerValue)
+        }
+    }
+
+    if (elCostValue === "") {
+        renderError('elcost-error-text', 'Electrical cost cannot be empty!')
+        errorOccured = true
+    }
+    else if (isNaN(Number(elCostValue))) {
+        renderError('elcost-error-text', 'Electrical cost must be a number')
+        errorOccured = true
+    }
+    else {
+        if (Number(elCostValue) < 0) {
+            renderError('elcost-error-text', 'Electrical cost cannot have a negative value!')
+            errorOccured = true
+        }
+        elCostValue = Number(elCostValue)
+    }
+
+    if (errorOccured) { 
+        return
+    }
+    else {
+        deleteErrorMessages()
+    }
+
+    let values = calculateProfitability(gpuPriceValue, hashrateValue, powerValue, elCostValue)
+
+    renderCalculatedValues(values)
 }
