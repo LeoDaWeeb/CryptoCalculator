849340d8dac34b44585da1ae10ecd721e10cb3e2
LeoDaWeeb <j13.laios@outlook.com.gr>
2021-11-25 03:24:22 +0200

    commit on master

--- js/settings/selectedColorTheme.js
+++ js/settings/selectedColorTheme.js
@@ -3,8 +3,10 @@ function changeSelectedColor() {
     let colors = ['red', 'green', 'blue', 'orange', 'purple', 'cyan']
     colors.forEach(color => {
         document.getElementById(`${color}-circle`).style.boxShadow = 'none'
+        document.getElementById(`${color}-circle`).style.borderColor = 'rgb(109, 109, 109)';
     })
     document.getElementById(`${colorTheme}-circle`).style.boxShadow = `0px 0px 15px 3px ${colorTheme}`
+    document.getElementById(`${colorTheme}-circle`).style.borderColor = getComputedStyle(document.querySelector(':root')).getPropertyValue(`--${colorTheme}-lighter`)
 }
 
 changeSelectedColor()
